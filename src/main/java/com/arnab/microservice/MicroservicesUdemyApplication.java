641556184561aa4dcccc4e78cb6ad31e06c68aaa
Arnab Banerjee <arnab.ban09@gmail.com>
2020-04-04 18:09:34 +0530

    Initial Commit

---
+++ src/main/java/com/arnab/microservice/MicroservicesUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.arnab.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesUdemyApplication.class, args);
+	}
+
+}
