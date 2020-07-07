eca285b18f7333321335c0068afb38a1e48c4790
Arnab Banerjee <arnab.ban09@gmail.com>
2020-07-07 18:31:42 +0530

    Added Eureka Server and Eureka Client.

---
+++ MicroserviceUdemyEurekaClient/src/test/java/com/arnab/microservice/eurekaclient/MicroserviceUdemyEurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arnab.microservice.eurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceUdemyEurekaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ MicroservicesUdemyEurekaServer/src/test/java/com/arnab/microservice/MicroservicesUdemyEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arnab.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesUdemyEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
